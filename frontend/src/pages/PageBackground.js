7c0a8ac7ef9925ab1969736ab88fd4e37a4503ac
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2023-05-31 18:24:55 +0200

    Dodany testowy komponent

---
+++ frontend/src/pages/PageBackground.js
@@ -0,0 +1,15 @@
+import React, { Children } from "react";
+import { Container } from "react-bootstrap";
+import './PageBackground.css';
+
+
+const PageBackground = () => {
+
+    return (
+        <Container id="PageBackground" fluid>
+            {Children}
+        </Container>
+    );
+};
+
+export default PageBackground;
